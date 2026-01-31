210348dacf4f9d1f81276dec0c4a946abdf8faa7
William Callahan <william@williamcallahan.com>
2026-01-30 23:20:47 -0800

    Update version to 0.1.6 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.williamcallahan"
-version = findProperty("version")?.toString()?.takeIf { it != "unspecified" } ?: "0.1.3-SNAPSHOT"
+version = findProperty("version")?.toString()?.takeIf { it != "unspecified" } ?: "0.1.6"
 
 java {
     toolchain {
